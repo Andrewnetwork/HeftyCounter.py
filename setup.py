e43c9e207ff58382a0d5d09e6110c76291342377
Andrew Ribeiro <andrewnetwork@gmail.com>
2017-11-21 15:03:30 -0500

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='HeftyNumber',
+   version='0.0.1',
+   description='HeftyNumber is a library built upon Numpy for doing arithmetic in arbitrary bases with arbitrary precision. ',
+   author='Andrew Riberio',
+   author_email='andrew@kexp.io',
+   packages=['HeftyNumber'],  #same as name
+   install_requires=['numpy'], #external packages as dependencies
+)
